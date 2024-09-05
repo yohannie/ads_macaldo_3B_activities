a41bb1b6abdad60e116c339f3c475bd1a6f2a888
john michael macaldo <johnmacaldo1103@gmail.com>
2024-09-05 15:17:32 +0800

    initial upload for activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
-
+use App\Models\Students;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/students/create', function () {
+    $student = new student (
 
-Route::get('/', function () {
-    return view('welcome');
+    );
+    $student->first_name = 'John_Michael';
+    $student->last_name = 'Macaldo';
+    $student->email = 'johnmacaldo1103@gmail.com';
+    $student->save();
+    return 'Student Created';
+});
+
+Route::get('/students', function () {
+    $students = student::all();
+    return $students;
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email', 'johnmacaldo1103@gmail.com')->first();
+    $student->email = 'johnmacaldo1103@gmail.com';
+    $student->save();
+    return 'Student Updated';
+});
+
+Route::get('/students/delete', function () {
+    $student = Student::where('email', 'johnmacaldo1103@gmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
 });
